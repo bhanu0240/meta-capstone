69b7424338f54e8d117c2400792381018eb5ad96
bhanu0240 <bhanureddy0240@gmail.com>
2025-12-23 16:13:35 +0530

    worked on booking logic and test cases

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "^react(.*)$": "<rootDir>/node_modules/react$1",
+    "^@testing-library/react$": "<rootDir>/node_modules/@testing-library/react",
+    "^@testing-library/jest-dom$":
+      "<rootDir>/node_modules/@testing-library/jest-dom",
+    "^@testing-library/user-event$":
+      "<rootDir>/node_modules/@testing-library/user-event",
+    "^react-router-dom$": "<rootDir>/node_modules/react-router-dom",
+    "^.+\\.(css|scss)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testMatch: ["**/?(*.)+(test).[jt]s?(x)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
+export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/layout/Layout.tsx";
 import Home from "./components/pages/Home";
 import NotFound from "./components/pages/NotFound";
 import UnderConstruction from "./components/pages/UnderConstruction";
+import Bookings from "./components/pages/Bookings/index.ts";
 import pages from "./utils/pages.ts";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             path={pages.get("menu").path}
             element={<UnderConstruction />}
           />
+          <Route path={pages.get("bookings").path} element={<Bookings />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
